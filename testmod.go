fe122ee1fc2f8b2a8ee91b235ae27e11d1e8bd19
caoyiming <joseph.yeeming.cho@gmail.com>
2021-10-25 18:38:19 +0800

    Version 2

--- testmod.go
+++ testmod.go
@@ -5,4 +5,14 @@ import "fmt"
 func SayHello(name string) {
 	fmt.Println("Hello,", name)
 	fmt.Println("GoodBye,", name)
+	switch lang {
+	case "en":
+		fmt.Println("English")
+	case "zh":
+		fmt.Println("Chinese")
+	case "fr":
+		fmt.Println("Francis")
+	default:
+		fmt.Println("Unknows")
+	}
 }
